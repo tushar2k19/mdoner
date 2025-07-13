6873aa01e1b4a28af8c05700bf8304855a5cdae7
Tushar <tushrrrocks@gmail.com>
2025-07-13 19:22:11 +0530

    adding frontend url in backend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080", "https://tushar-crm.netlify.app"
+    origins "http://localhost:8080", "https://mdoner.netlify.app"
 
     resource "*",
              headers: :any,
